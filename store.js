cdd7ac6f9f392e739b1ea3934ae157d9a5ce3f2e
Louis Cherel <cherel.louis@gmail.com>
2018-10-30 13:38:48 +0100

    initial commit

---
+++ store.js
@@ -0,0 +1,43 @@
+/**
+ * this is our database handler
+ * it takes time.
+ */
+async function findUser (id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: id
+      })
+    }, 2000)
+  })
+}
+
+class Store {
+  constructor () {
+    this._users = []
+  }
+
+  /**
+   * Step 1: if the user is already in our
+   * _users array, don't get it another time
+   * Step 2: if we don't, get it, and store
+   * it in our _users array
+   * Step 3: return it
+   */
+  async get (id) {
+    // check in our local cache
+    const existingUser = this._users
+      .find(u => u.id === id)
+
+    if (existingUser) {
+      return existingUser
+    }
+    // check in the database
+    const newUser = await findUser(id)
+
+    this._users.push(newUser)
+    return newUser
+  }
+}
+
+module.exports = Store
---
+++ test.js
@@ -0,0 +1,58 @@
+const Store = require('./store.js')
+
+async function shouldGetUser () {
+  const store = new Store()
+  const user = await store.get(1)
+
+  if (!user) {
+    throw new Error('User is not returned')
+  }
+  console.log('shouldGetUser succeded')
+}
+
+async function shouldGetUserTwice () {
+  const store = new Store()
+
+  // should take 2seconds
+  const user = await store.get(1)
+  if (!user) {
+    throw new Error('User is not returned')
+  }
+  const before = Date.now()
+  // should take < 2seconds
+  const user1 = await store.get(1)
+  if (!user1) {
+    throw new Error('User is not returned')
+  }
+  const now = Date.now()
+  if (now - before > 2000) {
+    throw new Error('Request takes too long')
+  }
+  console.log('shouldGetUserTwice succeded')
+}
+
+async function shouldGetUserInInterval () {
+  const store = new Store()
+
+  console.log(Date.now() / 1000)
+  // should take 2seconds
+  store.get(1)
+  .then(() => console.log(Date.now() / 1000))
+
+  // wait 1 second
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  // make the second call
+  store.get(1)
+  .then(() => console.log(Date.now() / 1000))
+}
+
+async function runTests () {
+  try {
+    // await shouldGetUser()
+    // await shouldGetUserTwice()
+    await shouldGetUserInInterval()
+  } catch (err) {
+    console.log('Tests failed', err)
+  }
+}
+runTests()
